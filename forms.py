bdb0e98b3e814a881a5c40acd7a4e1058032d040
abhayraina74 <abhayraina74@gmail.com>
2017-07-28 17:09:28 +0530

    Added "Like" and "Comment functions"

---
+++ forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from models import User, PostModel, Like, Comment
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=["username","full_name","email","password"]
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields= ["username", "password"]
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = PostModel
+        fields=['image', 'caption']
+
+
+class LikeForm(forms.ModelForm):
+    class Meta:
+        model = Like
+        fields = ['post']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment_text', 'post']
